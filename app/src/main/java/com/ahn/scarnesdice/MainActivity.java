c294930c58081c234f0e765b5efb703769d0f632
Jacob Lubecki <jacoblubecki@gmail.com>
2016-09-19 19:42:01 -0500

    Finish lesson 1.

--- app/src/main/java/com/ahn/scarnesdice/MainActivity.java
+++ app/src/main/java/com/ahn/scarnesdice/MainActivity.java
@@ -2,16 +2,30 @@ package com.ahn.scarnesdice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // UI Elements
+    TextView text;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = (TextView) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+    }
 
+    public void roll(View view) {
+        int rolledNumber = (int) (Math.random() * 6) + 1;
+
+        text.setText(String.valueOf(rolledNumber));
+        // text.setText(rolledNumber + "");
     }
 }
